a786f895fc3129580b1a68c34fe3cb245a7df871
Esteban C.v <stphgamer@gmail.com>
2025-08-28 19:56:12 -0500

    Project created
    adding the project

---
+++ hardware-pro/src/main/java/com/mine/hardware_pro/HardwareProApplication.java
@@ -0,0 +1,13 @@
+package com.mine.hardware_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardwareProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HardwareProApplication.class, args);
+	}
+
+}
---
+++ hardware-pro/src/test/java/com/mine/hardware_pro/HardwareProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mine.hardware_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardwareProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
