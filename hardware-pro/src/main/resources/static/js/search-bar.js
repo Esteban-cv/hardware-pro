1a2925d8d501dcacf970e4ba4330bf42549c474f
Esteban C.v <stphgamer@gmail.com>
2025-10-01 17:20:22 -0500

    Search bar edited now the inventory is in

--- hardware-pro/src/main/resources/static/js/search-bar.js
+++ hardware-pro/src/main/resources/static/js/search-bar.js
@@ -1,20 +1,12 @@
 $(document).ready(function() {
-    // Busca el select por su ID y lo inicializa como un Select2
     $('#moduleSearch').select2({
-        // Texto que aparece antes de seleccionar o escribir algo
         placeholder: "Buscar una sección...",
-        // Tema para que se vea como el resto de tus componentes Bootstrap 5
         theme: "bootstrap-5"
     });
 
-    // Evento que se dispara cuando el usuario selecciona una opción
     $('#moduleSearch').on('select2:select', function (e) {
-        // Obtenemos los datos de la opción seleccionada
         var data = e.params.data;
-
-        // 'data.id' contiene el valor del atributo 'value' de la opción (la URL)
         if (data.id) {
-            // Redirigimos el navegador a esa URL
             window.location.href = data.id;
         }
     });
